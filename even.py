f9cc0373c3d71d4e62a8103cc1877119dc92e49b
Egor <68809735+ZOrtanin@users.noreply.github.com>
2022-07-16 14:14:06 +0300

    Update even.py

--- even.py
+++ even.py
@@ -3,11 +3,13 @@ import time
 
 
 def isEven(value):
+	''' Пример из задачи '''
     start_time = datetime.now()
     print(value%2 == 0)
     print(datetime.now() - start_time)
 
 def myEven(value):
+	''' Пример решения если проверяемые значения будут меньше 500 и не нужно будет использовать значния с плавающей запятой '''
 	start_time = datetime.now()
 	while True:
 		
